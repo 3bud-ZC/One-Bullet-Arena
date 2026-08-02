392cf84062226d169c91485b0273a122d0c7c118
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-02 04:50:44 +0300

    chore: start Arabic game runtime immediately

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { OneBulletArena } from './game.js';
 
 const canvas = document.querySelector('#game-canvas');
 if (!(canvas instanceof HTMLCanvasElement)) {
-  throw new Error('Game canvas was not found.');
+  throw new Error('تعذر العثور على لوحة اللعبة.');
 }
 
 new OneBulletArena(canvas);

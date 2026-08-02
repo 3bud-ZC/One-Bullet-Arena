6866b363167537bdbf0b5312d106e3e0c1509ce0
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-02 07:33:54 +0300

    feat: install replayability expansion runtime

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { attachStabilizationControls, installStabilization } from './stabilizati
 import { installDefeatUiRefine } from './defeat-ui-refine.js';
 import { installVisualIdentity } from './visual-identity.js';
 import { attachProgressionControls, installProgression } from './progression.js';
+import { attachReplayabilityControls, installReplayability } from './replayability.js';
 
 installPhysicalKeyboardBridge();
 installUiPolish(OneBulletArena);
@@ -14,6 +15,7 @@ installStabilization(OneBulletArena);
 installDefeatUiRefine(OneBulletArena);
 installVisualIdentity(OneBulletArena);
 installProgression(OneBulletArena);
+installReplayability(OneBulletArena);
 
 const canvas = document.querySelector('#game-canvas');
 if (!(canvas instanceof HTMLCanvasElement)) {
@@ -27,3 +29,4 @@ const game = new OneBulletArena(canvas);
 attachPresentationControls(game);
 attachStabilizationControls(game);
 attachProgressionControls(game);
+attachReplayabilityControls(game);

88f9b382173790172f980bf9ee6962a254d03b06
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-06 01:43:39 +0300

    refactor: boot the modular runtime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { OneBulletGame } from './game.js';
+import { OneBulletRuntime } from './game-runtime.js';
 
 migrateLegacyStorage();
 
@@ -9,7 +9,7 @@ if (!(canvas instanceof HTMLCanvasElement)) throw new Error('تعذر العثو
 canvas.tabIndex = 0;
 canvas.addEventListener('pointerdown', () => canvas.focus());
 
-const game = new OneBulletGame(canvas, liveRegion);
+const game = new OneBulletRuntime(canvas, liveRegion);
 const qaMode = new URLSearchParams(location.search).get('qa') === '1';
 if (qaMode) window.__ONE_BULLET_ARENA__ = game;
 

6a05e52ac0a9980328ee5da1978498fe0ea9cc83
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-05 03:59:16 +0300

    refactor: remove archived gameplay systems from simple release

--- src/mobile-ui-style-loader.js
+++
@@ -1,9 +0,0 @@
-const STYLESHEET_ID = 'mobile-ui-stabilization-styles';
-
-if (typeof document !== 'undefined' && !document.getElementById(STYLESHEET_ID)) {
-  const link = document.createElement('link');
-  link.id = STYLESHEET_ID;
-  link.rel = 'stylesheet';
-  link.href = './mobile-ui-stabilization.css';
-  document.head.append(link);
-}

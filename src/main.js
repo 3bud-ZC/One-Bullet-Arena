166b996f6b8d30f09b5079e663b977e843541cb0
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-06 06:36:31 +0300

    fix: boot the movement hotfix runtime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { OneBulletPolishRuntime } from './polish-runtime.js';
+import { OneBulletMovementHotfixRuntime } from './movement-hotfix-runtime.js';
 
 migrateLegacyStorage();
 
@@ -9,7 +9,7 @@ if (!(canvas instanceof HTMLCanvasElement)) throw new Error('تعذر العثو
 canvas.tabIndex = 0;
 canvas.addEventListener('pointerdown', () => canvas.focus());
 
-const game = new OneBulletPolishRuntime(canvas, liveRegion);
+const game = new OneBulletMovementHotfixRuntime(canvas, liveRegion);
 const qaMode = new URLSearchParams(location.search).get('qa') === '1';
 if (qaMode) window.__ONE_BULLET_ARENA__ = game;
 

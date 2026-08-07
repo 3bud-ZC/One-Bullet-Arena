01a64aafd164d4b6f880cbb40fd2575b542f38ab
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-07 17:17:57 +0300

    feat: activate combat juice runtime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { OneBulletVisualOverhaulRuntime } from './core/visual-overhaul-runtime.js';
+import { OneBulletCombatJuiceRuntime } from './core/combat-juice-runtime.js';
 import { RELEASE_INFO } from './release.js';
 
 migrateLegacyStorage();
@@ -10,7 +10,7 @@ if (!(canvas instanceof HTMLCanvasElement)) throw new Error('تعذر العثو
 canvas.tabIndex = 0;
 canvas.addEventListener('pointerdown', () => canvas.focus());
 
-const game = new OneBulletVisualOverhaulRuntime(canvas, liveRegion);
+const game = new OneBulletCombatJuiceRuntime(canvas, liveRegion);
 const qaMode = new URLSearchParams(location.search).get('qa') === '1';
 if (qaMode) {
   window.__ONE_BULLET_ARENA__ = game;

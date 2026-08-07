f03904fc3731f9ee16a9cb871fd20f12f28d3cf5
Abdullah Ragab <s-abdallah.ali@zewailcity.edu.eg>
2026-08-08 01:45:00 +0300

    feat: boot art direction runtime

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { OneBulletVisualOverhaulRuntime } from './core/visual-overhaul-runtime.js';
+import { OneBulletArtDirectionRuntime } from './core/art-direction-runtime.js';
 import { RELEASE_INFO } from './release.js';
 
 migrateLegacyStorage();
@@ -10,7 +10,7 @@ if (!(canvas instanceof HTMLCanvasElement)) throw new Error('تعذر العثو
 canvas.tabIndex = 0;
 canvas.addEventListener('pointerdown', () => canvas.focus());
 
-const game = new OneBulletVisualOverhaulRuntime(canvas, liveRegion);
+const game = new OneBulletArtDirectionRuntime(canvas, liveRegion);
 const qaMode = new URLSearchParams(location.search).get('qa') === '1';
 if (qaMode) {
   window.__ONE_BULLET_ARENA__ = game;
